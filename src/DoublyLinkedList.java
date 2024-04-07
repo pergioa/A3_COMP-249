5f3711afb52e68e24cfd014bc906318edd39b85f
DaKingzz <artsanes68@gmail.com>
2024-04-07 16:31:08 -0400

    Linked List and Doubly Linked List

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,150 @@
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int counter;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+        counter = 0;
+    }
+
+    // Add at head
+    public void addAtHead(int value) {
+        Node temp = head;
+        head = new Node(value, null, head);
+
+        if (tail == null) {
+            tail = head;
+        } else {
+            temp.before = head;
+        }
+
+        counter++;
+    }
+
+    // Add at tail
+    public void addAtTail(int value) {
+        Node temp = tail;
+        tail = new Node(value, tail, null);
+        if (head == null) {
+            head = tail;
+        } else {
+            temp.after = tail;
+        }
+
+        counter++;
+    }
+
+    // Add after (while going from the head to tail)
+    public void addAfter(int referenceValue, int newValue) {
+        Node position = head;
+        while (position != null && position.value != referenceValue) {
+            position = position.after;
+        }
+        if (position != null && position.value == referenceValue) {
+            // if the ref value is the last element
+            if (position.after == null) {
+                addAtTail(newValue);
+            } else {
+                Node n = new Node(newValue, null, null);
+                n.after = position.after;
+                n.before = position;
+                position.after.before = n;
+                position.after = n;
+                counter++;
+            }
+        }
+    }
+
+    // Remove head
+    public int removeHead() {
+        if (head == null) {
+            return -1;
+        } else if (head == tail) {
+            Node temp = head;
+            head = null;
+            tail = null;
+            counter--;
+            return temp.value;
+        } else {
+            Node temp = head;
+            head = head.after;
+            head.before = null;
+            counter--;
+            return temp.value;
+        }
+    }
+
+    // Remove tail
+    public int removeTail() {
+        if (tail == null) {
+            return -1;
+        } else if (head == tail) {
+            Node temp = head;
+            head = null;
+            tail = null;
+            counter--;
+            return temp.value;
+        } else {
+            Node temp = tail;
+            tail = tail.before;
+            tail.after = null;
+            counter--;
+            return temp.value;
+        }
+    }
+
+    // Remove value
+    // Remove after value
+    // Remove before value
+    // get size
+    public int getSize() {
+        return counter;
+    }
+
+    // Display
+    public void displayFromHeadToTail() {
+        if (head == null) {
+            System.out.println("The list is empty");
+        } else {
+            System.out.println("Displaying from Head to Tail with " + counter + " element(s).");
+            Node position = head;
+
+            while (position != null) {
+                System.out.println(position.value);
+                position = position.after;
+            }
+        }
+    }
+
+    public void displayFromTailToHead() {
+        if (tail == null) {
+            System.out.println("The list is empty.");
+        } else {
+            System.out.println("Displaying from Tail to Head " + counter + " element(s).");
+            Node position = tail;
+            while (position != null) {
+                System.out.println(position.value);
+                position = position.before;
+            }
+        }
+    }
+
+    private class Node {
+
+        // Data
+        private int value;
+        // Links
+        private Node after;
+        private Node before;
+
+        public Node(int value, Node before, Node after) {
+            this.value = value;
+            this.before = before;
+            this.after = after;
+        }
+    }
+
+}
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,180 @@
 public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    // Optional
+    private int counter;
+
+    public LinkedList() {
+        head = null;
+        counter = 0;
+    }
+
+    // Add to head
+    public void addAtHead(int value) {
+        head = new Node(value, head);
+        counter++;
+    }
+
+    // Add to end
+    public void addAtEnd(int value) {
+        if (head == null) {
+            addAtHead(value);
+        } else {
+            Node position = head;
+            while (position.next != null) {
+                position = position.next;
+            }
+
+            Node n = new Node(value, null);
+            position.next = n;
+
+            counter++;
+        }
+    }
+
+    // Add after a value
+    public void addAfter(int referenceValue, int value) {
+
+        Node position = head;
+
+        // Handle the case when referenceValue does not exist
+        while (position != null && position.value != referenceValue) {
+            position = position.next;
+        }
+
+        if (position == null) { // value does not exist
+            return;
+        } else {
+            Node n = new Node(value, position.next);
+            position.next = n;
+            counter++;
+        }
+
+    }
+
+    // Add before a value. WE ONLY ADD IF THE REFERENCEVALUE exists
+    public void addBefore(int referenceValue, int newValue) {
+        if (head == null) {
+            return;
+        }
+        if (head.value == referenceValue) {
+            addAtHead(newValue);
+        } else {
+            Node position = head;
+
+            while (position.next != null && position.next.value != referenceValue) {
+                position = position.next;
+            }
+
+            if (position.next != null && position.next.value == referenceValue) {
+                position.next = new Node(newValue, position.next);
+                counter++;
+            }
+        }
+    }
+
+    // Remove head
+    public int removeHead() {
+        if (head != null) {
+            Node temp = head;
+            head = head.next;
+            counter--;
+            temp.next = null;
+            return temp.value;
+        }
+        return -1; // Or throw an Exception
+    }
+
+    // Remove tail/last one
+    public int removeLast() {
+        if (head == null) {
+            return -1;
+        } else if (head.next == null) {
+            return removeHead();
+        } else {
+            Node position = head;
+            while (position.next.next != null) {
+                position = position.next;
+            }
+            Node temp = position.next;
+            position.next = null;
+            counter--;
+            return temp.value;
+        }
+    }
+
+    // Remove a value
+    public int removeValue(int value) {
+        if (head == null) {
+            return - 1;
+        }
+        if (head.value == value) {
+            return removeHead();
+        } else {
+            Node position = head;
+
+            while (position.next != null && position.next.value != value) {
+                position = position.next;
+            }
+            if (position.next.value == value) {
+                Node temp = position.next;
+                position.next = position.next.next;
+                counter--;
+                return temp.value;
+            }
+        }
+        return -1;
+    }
+
+    // Remove after a value
+    public int removeAfter(int value) {
+        if (head == null) {
+            return -1;
+        } else {
+            Node position = head;
+            while (position != null && position.value != value) {
+                position = position.next;
+            }
+            if (position != null && position.next != null) {
+                Node temp = position.next;
+                position.next = position.next.next;
+                return temp.value;
+            }
+        }
+        return -1;
+    }
+
+    // get size
+    public int getSize() {
+        return counter;
+    }
+
+    // Display
+    public void display() {
+        if (head == null) {
+            System.out.println("Your list is empty.");
+        } else {
+            System.out.println("Here are your " + counter + " values from the list: ");
+            Node position = head;
+            while (position != null) {
+                System.out.println(position.value);
+                position = position.next;
+
+            }
+        }
+    }
+
+    private class Node {
+
+        // Data
+        private int value;
+        // Link
+        private Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 }

8e7be2be470c462e4778b5f84dfa9a10b6ef7e9f
DaKingzz <artsanes68@gmail.com>
2024-04-14 15:11:58 -0400

    Comments done for LikedList and Vocab

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,14 +1,38 @@
+// -----------------------------------------------------
+// Assignment: 3, COMP249
+// Parts: LinkedList
+// Written by: Sergio Abreo Alvarez - 40274677 , Arturo Sanchez Escobar - 40283236
+// Purpose: This class is the model for a single LinkedList, it handles all necessary manipulations of its elements
+// Due date: April 15, 2024
+// -----------------------------------------------------
+
+/**
+ * <p> Assignment 3, COMP249</p>
+ * @author Arturo Sanchez - 40283236
+ * @author Sergio Abreo - 40274677
+ * @version 1.0.0
+ * <p> This class is the model for a single LinkedList, it handles all necessary manipulations of its elements</p>
+ * <p>Due date: April 27, 2024</p>
+ */
 public class LinkedList {
 
     private Node head;
 
     private int counter;
 
+    /**
+     * Builds a LinkedList object with no elements
+     */
     public LinkedList() {
         head = null;
         counter = 0;
     }
 
+    /**
+     * Get the value of an element in the LinkedList
+     * @param index of the value to be found
+     * @return value of the node at the specified index
+     */
     public String get(int index) {
         Node temp = head;
         if (index >= counter){
@@ -20,6 +44,11 @@ public class LinkedList {
         return temp.value;
     }
 
+    /**
+     * Gets the index of a value
+     * @param value of the index to be found
+     * @return index of the specified value in the LinkedList
+     */
     public int indexOf(String value){
         int index = -1;
         Node temp = head;
@@ -31,16 +60,21 @@ public class LinkedList {
             temp = temp.next;
         }
         return index;
-
     }
 
-    // Add to head
+    /**
+     * Add a node at the head of the LinkedList
+     * @param value of the new head
+     */
     public void addAtHead(String value) {
         head = new Node(value, head);
         counter++;
     }
 
-    // Add to end
+    /**
+     * Add a tail, a node at the ned of the LinkedList
+     * @param value of the new node/tail to add
+     */
     public void addAtEnd(String value) {
         if (head == null) {
             addAtHead(value);
@@ -49,7 +83,6 @@ public class LinkedList {
             while (position.next != null) {
                 position = position.next;
             }
-
             Node n = new Node(value, null);
             position.next = n;
 
@@ -57,16 +90,17 @@ public class LinkedList {
         }
     }
 
-    // Add after a value
+    /**
+     * Add after a node after a specified value
+     * @param referenceValue the value from which its next needs to added a new node
+     * @param value of the new node to add
+     */
     public void addAfter(String referenceValue, String value) {
-
         Node position = head;
-
         // Handle the case when referenceValue does not exist
         while (position != null && position.value != referenceValue) {
             position = position.next;
         }
-
         if (position == null) { // value does not exist
             return;
         } else {
@@ -74,10 +108,15 @@ public class LinkedList {
             position.next = n;
             counter++;
         }
-
     }
 
     // Add before a value. WE ONLY ADD IF THE REFERENCEVALUE exists
+
+    /**
+     * Add a node before the specified value in the LinkedList
+     * @param referenceValue of the node to add the new node before of
+     * @param newValue is the value of the new node
+     */
     public void addBefore(String referenceValue, String newValue) {
         if (head == null) {
             return;
@@ -98,7 +137,10 @@ public class LinkedList {
         }
     }
 
-    // Remove head
+    /**
+     * Remove the head of the LinkedList
+     * @return the value of the removed head
+     */
     public String removeHead() {
         if (head != null) {
             Node temp = head;
@@ -110,7 +152,10 @@ public class LinkedList {
         return ""; // Or throw an Exception
     }
 
-    // Remove tail/last one
+    /**
+     * Remove the tail of the LinkedList
+     * @return the value of the removed tail
+     */
     public String removeLast() {
         if (head == null) {
             return "";
@@ -128,6 +173,11 @@ public class LinkedList {
         }
     }
 
+    /**
+     * Changes the value of a node at the specified index in the LinkedList
+     * @param index of the value to update
+     * @param value of that needs to be aaded at the specified index
+     */
     public void updateIndexValue(int index, String value){
         Node temp = head;
         for(int i = 0 ; i != index ; i++){
@@ -135,7 +185,12 @@ public class LinkedList {
         }
         temp.value = value;
     }
-    // Remove a value
+
+    /**
+     *  Remove a value in the LinkedList
+     * @param value to be removed
+     * @return return the next node after the removal
+     */
     public String removeValue(String value) {
         if (head == null) {
             return "";
@@ -154,12 +209,15 @@ public class LinkedList {
                 }
                 position = position.next;
             }
-
         }
         return "";
     }
 
-    // Remove after a value
+    /**
+     * Removes the node after a specified value
+     * @param value and its next is to be removed
+     * @return the node that comes after the one removed
+     */
     public String removeAfter(String value) {
         if (head == null) {
             return "";
@@ -177,12 +235,17 @@ public class LinkedList {
         return "";
     }
 
-    // get size
+    /**
+     * Returns the size of the list
+     * @return the size of the list
+     */
     public int getSize() {
         return counter;
     }
 
-    // Display
+    /**
+     * Displays the whole list
+     */
     public void display() {
         if (head == null) {
             System.out.println("Your list is empty.");
@@ -197,13 +260,19 @@ public class LinkedList {
         }
     }
 
+    /**
+     * This is the basic model for all elements of the single LinkedList, it holds the necessary attributes
+     */
     private class Node {
-
         // Data
         private String value;
         // Link
         private Node next;
-
+        /**
+         * Builds a object Node with its data and next node's reference
+         * @param value of the node
+         * @param next is the next node in the list
+         */
         public Node(String value, Node next) {
             this.value = value;
             this.next = next;
--- src/Vocab.java
+++ src/Vocab.java
@@ -1,17 +1,48 @@
+
+// -----------------------------------------------------
+// Assignment: 3, COMP249
+// Parts: LinkedList
+// Written by: Sergio Abreo Alvarez - 40274677 , Arturo Sanchez Escobar - 40283236
+// Purpose: This class handles the creation of new Vocab objects and LinkedLists. It handles everything related to the topic of a Vocab
+// and its words through the usage of the LinkedList
+// Due date: April 15, 2024
+// -----------------------------------------------------
 import java.util.ArrayList;
 
+/**
+ * <p> Assignment 3, COMP249</p>
+ * @author Arturo Sanchez - 40283236
+ * @author Sergio Abreo - 40274677
+ * @version 1.0.0
+ * <p> This class handles the creation of new Vocab objects and LinkedLists. It handles everything related to the topic of a Vocab
+ * and its words through the usage of the LinkedList</p>
+ * <p>Due date: April 27, 2024</p>
+ */
 public class Vocab {
     String topic;
     LinkedList words;
 
+    /**
+     * Get the topic of Vocab
+     * @return the topic Vocab
+     */
     public String getTopic(){
         return topic;
     }
 
+    /**
+     * Sets the topic of Vocab
+     * @param value is the topic of Vocab
+     */
     public void setTopic(String value){
         topic = value;
     }
 
+    /**
+     * Builds the Vocab object with all of its specified values
+     * @param topic is the topic of Vocab
+     * @param words is the list words of Vocab
+     */
     public Vocab(String topic, String[] words){
         this.topic = topic;
         this.words = new LinkedList();
@@ -19,6 +50,11 @@ public class Vocab {
             this.words.addAtHead(s);
     }
 
+    /**
+     * This methods filters all words in the vocab for those that start with a specified letter
+     * @param l is the character to filter by
+     * @return the arrayList of all words found
+     */
     public ArrayList<String> wordsBeginningWith(char l){
         ArrayList<String> wordsFound = new ArrayList<>();
         for(int i = 0; i<words.getSize(); i++){
@@ -29,6 +65,11 @@ public class Vocab {
         return wordsFound;
     }
 
+    /**
+     * This method compares two Vocab ojbect to see if they are the same
+     * @param obj is the object to compare with
+     * @return a boolean saying if the equality is true or not
+     */
     public boolean equals(Object obj){
         if(obj == null || obj.getClass() != this.getClass())
             return false;
@@ -47,6 +88,10 @@ public class Vocab {
         return isEqual;
     }
 
+    /**
+     * Display the attributes of the ojbect Vocab
+     * @return a string of all the current Vocab attributes
+     */
     @Override
     public String toString() {
         return "Vocab{ topic:'" + topic + ", words:" + words + '}';
